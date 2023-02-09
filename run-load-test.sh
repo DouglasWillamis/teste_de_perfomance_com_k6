a5f4d43b915c0b7b46066d3b971dd07eb1799b2a
Douglas Willamis <douglaswillamis@gmail.com>
2023-02-09 10:21:02 -0300

    chore: adicionando configurações para roda os scripts na K6 cloud

--- run-load-test.sh
+++ run-load-test.sh
@@ -3,3 +3,5 @@ echo "--------------------------------------------------------------------------
 echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
 echo "--------------------------------------------------------------------------------------"
 docker-compose run --rm k6 run /scripts/"$1".js
+# If you need to run on k6 cloud use this command docker-compose run --rm k6 cloud /scripts/"$1".js
+# Or try this docker-compose run --rm k6 run -o cloud /scripts/"$1".js
